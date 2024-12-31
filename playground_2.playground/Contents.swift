013c1c9457664c1710ce8fb79b597d806ddd5114
yohans <rhdygks231@naver.com>
2024-12-31 09:50:48 +0900

    optional code

--- playground_2.playground/Contents.swift
+++ playground_2.playground/Contents.swift
@@ -1,1 +1,31 @@
+import Foundation
+
+func main(){
+    var dayOfTheWeek: String = "Monday"
+    print(dayOfTheWeek)
+    dayOfTheWeek = "Tuesday"
+    print(dayOfTheWeek)
+    
+    var numberOfFingerHeldUpFinn : Int?
+    numberOfFingerHeldUpFinn = 1
+    print(numberOfFingerHeldUpFinn!)
+    numberOfFingerHeldUpFinn = nil
+    //print(numberOfFingerHeldUpFinn ?? "niiiiiiiil")
+    
+    //var lastNumberOfFingersHeldUpByFinn : Int = numberOfFingerHeldUpFinn!
+    //print(lastNumberOfFingersHeldUpByFinn)
+    
+    if let numberOfFingerHeld = numberOfFingerHeldUpFinn{
+        print(numberOfFingerHeld)
+    }
+    else {print("모름")}
+    
+    //guard let numberOfFingerHeld = numberOfFingerHeldUpFinn else{
+    //    print("모름요")
+    //}
+    //print(numberOfFingerHeld)
+}
+
+main()
+
 
